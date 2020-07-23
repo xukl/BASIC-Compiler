b74ebfadf49122d52d5d7c0ab821a96a30eabe91
xukl <xu2k3l4@outlook.com>
2020-07-23 12:11:52 +0800

    FIX expr.cc

--- src/expr.cc
+++ src/expr.cc
@@ -1,3 +1,4 @@
+#include <ostream>
 #include <string>
 #include <utility>
 #include <memory>
@@ -8,34 +9,62 @@ using expr_ite = std::string::const_iterator;
 using value_type = int;
 struct expr
 {
-	virtual ~expr();
+	virtual ~expr() {}
+	virtual void print(std::ostream &) const = 0;
 };
+std::ostream &operator<< (std::ostream &os, const expr &x)
+{
+	x.print(os);
+	return os;
+}
 struct bin_op : expr
 {
+	virtual const char *op_name() const = 0;
 	std::unique_ptr<expr> lc, rc;
 	bin_op(std::unique_ptr<expr> &&_l, std::unique_ptr<expr> &&_r)
 		: lc(std::move(_l)), rc(std::move(_r)) {}
+	void print(std::ostream &os) const
+	{
+		os << '{' << op_name() << ' ' << *lc << ' ' << *rc << '}';
+	}
 };
 struct unary_op : expr
 {
+	virtual const char *op_name() const = 0;
 	std::unique_ptr<expr> c;
 	unary_op(std::unique_ptr<expr> &&_c) : c(std::move(_c)) {}
+	void print(std::ostream &os) const
+	{
+		os << '{' << op_name() << ' ' << *c << '}';
+	}
 };
 struct id : expr
 {
 	std::string id_name;
-	template <typename ...Args>
-	id(Args&&... args) : id_name(std::forward<Args>(args)...) {}
+	id(std::string &&_id_name) : id_name(std::move(_id_name)) {}
+	id(const std::string &_id_name) : id_name(_id_name) {}
+	void print(std::ostream &os) const
+	{
+		os << "{var " << id_name << '}';
+	}
 };
 struct imm_num : expr
 {
 	value_type value;
 	imm_num(const value_type &v) : value(v) {}
+	void print(std::ostream &os) const
+	{
+		os << "{imm " << value << '}';
+	}
 };
 #define STRUCT_BIN(name)\
 struct name : bin_op\
 {\
 	using bin_op::bin_op;\
+	const char *op_name() const\
+	{\
+		return #name;\
+	}\
 };
 STRUCT_BIN(add)
 STRUCT_BIN(sub)
@@ -43,17 +72,25 @@ STRUCT_BIN(mul)
 STRUCT_BIN(div)
 struct subscript : expr
 {
-	std::unique_ptr<id> arr;
+	std::unique_ptr<expr> arr;
 	std::unique_ptr<expr> idx;
-	subscript(std::unique_ptr<id> &&_arr, std::unique_ptr<expr> &&_idx)
+	subscript(std::unique_ptr<expr> &&_arr, std::unique_ptr<expr> &&_idx)
 		: arr(std::move(_arr)), idx(std::move(_idx)) {}
+	void print(std::ostream &os) const
+	{
+		os << "{subscript " << *arr << ' ' << *idx << '}';
+	}
 };
 struct neg : unary_op
 {
 	using unary_op::unary_op;
+	const char *op_name() const
+	{
+		return "neg";
+	}
 };
 
-using parse_expr_result = std::pair<std::unique_ptr<expr>, expr_ite>;
+using parse_expr_result = std::unique_ptr<expr>;
 using parse_id_result = std::pair<std::unique_ptr<id>, expr_ite>;
 inline void skip_space(expr_ite &ptr, const expr_ite &end)
 {
@@ -63,35 +100,31 @@ inline void skip_space(expr_ite &ptr, const expr_ite &end)
 /*
  * {E0} ::= ({expr}) | {id} | {IMM}
  * {E1} ::= {E0} | {id}[{expr}]
- * {E2} ::= {E1} | +{E1} | -{E1}
- * {E3} ::= {E2} | {E2} * {E2} | {E2} / {E2}
- * {expr} ::= {E3} | {E3} + {E3} | {E3} - {E3}
+ * {E2} ::= {E1} | +{E2} | -{E2}
+ * {E3} ::= {E2} | {E3} * {E2} | {E3} / {E2}
+ * {expr} ::= {E3} | {expr} + {E3} | {expr} - {E3}
  */
-parse_expr_result parse_expr(expr_ite first, const expr_ite &last);
-std::pair<std::unique_ptr<id>, expr_ite>
-parse_id(expr_ite first, const expr_ite &last)
+std::unique_ptr<expr> parse_expr(expr_ite &first, const expr_ite &last);
+id parse_id(expr_ite &first, const expr_ite &last)
 {
 	skip_space(first, last);
-	auto begin_name = first;
+	auto name_begin = first;
 	while (first < last && isalpha(*first))
 		++first;
-	return std::pair<std::unique_ptr<id>, expr_ite>
-		(std::make_unique<id>(begin_name, first), first);
+	return std::string(name_begin, first);
 }
-std::pair<std::unique_ptr<imm_num>, expr_ite>
-parse_unsigned_num(expr_ite first, const expr_ite &last)
+imm_num parse_unsigned_num(expr_ite &first, const expr_ite &last)
 {
 	skip_space(first, last);
-	value_type value;
+	value_type value = 0;
 	while (first < last && isdigit(*first))
 	{
 		value = value * 10 + *first - '0';
 		++first;
 	}
-	return std::pair<std::unique_ptr<imm_num>, expr_ite>
-		(std::make_unique<imm_num>(value), first);
+	return imm_num(value);
 }
-parse_expr_result parse_e0(expr_ite first, const expr_ite &last)
+std::unique_ptr<expr> parse_e0(expr_ite &first, const expr_ite &last)
 {
 	skip_space(first, last);
 	if (first == last)
@@ -99,138 +132,166 @@ parse_expr_result parse_e0(expr_ite first, const expr_ite &last)
 	if (*first == '(')
 	{
 		++first;
-		auto &&tmp = parse_expr(first, last);
-		first = tmp.second;
+		auto &&expr = parse_expr(first, last);
 		skip_space(first, last);
 		if (first < last && *first == ')')
-			return parse_expr_result(std::move(tmp.first), first + 1);
+		{
+			++first;
+			return std::move(expr);
+		}
 		throw ":-( expected ')'.";
 	}
 	else if (isdigit(*first))
-		return parse_unsigned_num(first, last);
+		return std::make_unique<imm_num>(parse_unsigned_num(first, last));
 	else if (isalpha(*first))
-		return parse_id(first, last);
+		return std::make_unique<id>(parse_id(first, last));
 	else
 		throw "Error at parse_e0. Probably caused by invalid character.";
 }
-parse_expr_result parse_e1(expr_ite first, const expr_ite &last)
+std::unique_ptr<expr> parse_e1(expr_ite &first, const expr_ite &last)
 {
 	skip_space(first, last);
 	if (first == last)
 		throw "The expression is incomplete.";
-	if (isalpha(*first))
-	{
-		auto &&id = parse_id(first, last);
-		first = id.second;
-		skip_space(first, last);
-		if (first < last && *first == '[')
-		{
-			auto &&idx = parse_expr(first, last);
-			first = idx.second;
-			skip_space(first, last);
-			if (first < last && *first == ']')
-				return parse_expr_result(std::make_unique<subscript>
-						(std::move(id.first), std::move(idx.first)), first + 1);
-			throw ":-( unclosed '['.";
-		}
-		return parse_expr_result(std::move(id.first), first + 1);
-	}
-	return parse_e0(first, last);
-}
-parse_expr_result parse_e2(expr_ite first, const expr_ite &last)
-{
+	auto &&ret = parse_e0(first, last);
 	skip_space(first, last);
-	if (first == last)
-		throw "The expression is incomplete.";
-	if (*first == '-')
+	while (first < last && *first == '[')
 	{
 		++first;
-		auto tmp = parse_e1(first, last);
-		return parse_expr_result(std::make_unique<neg>(std::move(tmp.first)), tmp.second);
+		auto &&idx = parse_expr(first, last);
+		skip_space(first, last);
+		if (first == last || *first != ']')
+			throw ":-( unclosed '['.";
+		++first;
+		skip_space(first, last);
+		ret = std::make_unique<subscript>(std::move(ret), std::move(idx));
 	}
-	else
-	{
-		if (*first == '+')
-			++first;
-		return parse_e1(first, last);
-	}
+	return std::move(ret);
 }
-parse_expr_result parse_e3(expr_ite first, const expr_ite &last)
+std::unique_ptr<expr> parse_e2(expr_ite &first, const expr_ite &last)
 {
 	skip_space(first, last);
-	auto ret = parse_e2(first, last);
-	first = ret.second;
+	if (first == last)
+		throw "The expression is incomplete.";
+	int sign = +1;
+	while (*first == '+' || *first == '-')
+	{
+		if (*first == '-')
+			sign = -sign;
+		++first;
+		skip_space(first, last);
+	}
+	auto &&e1 = parse_e1(first, last);
+	if (sign == +1)
+		return std::move(e1);
+	return std::make_unique<neg>(std::move(e1));
+}
+std::unique_ptr<expr> parse_e3(expr_ite &first, const expr_ite &last)
+{
+	skip_space(first, last);
+	auto &&ret = parse_e2(first, last);
 	skip_space(first, last);
 	while (first < last && (*first == '*' || *first == '/'))
 	{
 		char op_tmp = *first;
 		++first;
-		auto tmp = parse_e2(first, last);
-		first = tmp.second;
+		auto &&tmp = parse_e2(first, last);
 		skip_space(first, last);
 		if (op_tmp == '*')
-			ret = parse_expr_result(std::make_unique<mul>
-					(std::move(ret.first), std::move(tmp.first)),
-				 tmp.second);
+			ret = std::make_unique<mul>(std::move(ret), std::move(tmp));
 		else
-			ret = parse_expr_result(std::make_unique<div>
-					(std::move(ret.first), std::move(tmp.first)),
-				 tmp.second);
+			ret = std::make_unique<div>(std::move(ret), std::move(tmp));
 	}
-	return parse_expr_result(std::move(ret.first), first);
+	return std::move(ret);
 }
-parse_expr_result parse_expr(expr_ite first, const expr_ite &last)
+std::unique_ptr<expr> parse_expr(expr_ite &first, const expr_ite &last)
 {
 	skip_space(first, last);
-	auto ret = parse_e3(first, last);
-	first = ret.second;
+	auto &&ret = parse_e3(first, last);
 	skip_space(first, last);
 	while (first < last && (*first == '+' || *first == '-'))
 	{
 		char op_tmp = *first;
 		++first;
-		auto tmp = parse_e3(first, last);
-		first = tmp.second;
+		auto &&tmp = parse_e3(first, last);
 		skip_space(first, last);
 		if (op_tmp == '+')
-			ret = parse_expr_result(std::make_unique<add>
-					(std::move(ret.first), std::move(tmp.first)),
-				 tmp.second);
+			ret = std::make_unique<add>(std::move(ret), std::move(tmp));
 		else
-			ret = parse_expr_result(std::make_unique<sub>
-					(std::move(ret.first), std::move(tmp.first)),
-				 tmp.second);
+			ret = std::make_unique<sub>(std::move(ret), std::move(tmp));
 	}
-	return parse_expr_result(std::move(ret.first), first);
+	return std::move(ret);
+}
+std::unique_ptr<expr> parse_expr(const std::string &expr_str)
+{
+	auto first = expr_str.cbegin();
+	const auto last = expr_str.cend();
+	auto &&ret = parse_expr(first, last);
+	skip_space(first, last);
+	if (first != last)
+		throw "Extra trailing characters.";
+	return std::move(ret);
 }
 
 struct bool_expr
 {
 	virtual ~bool_expr() {}
+	virtual void print(std::ostream &) const = 0;
 };
+std::ostream &operator<< (std::ostream &os, const bool_expr &x)
+{
+	x.print(os);
+	return os;
+}
 struct cmp : bool_expr
 {
 	const enum cmp_op { LT, LE, GT, GE, EQ, NE } op;
 	std::unique_ptr<expr> lc, rc;
 	cmp(cmp_op _op, std::unique_ptr<expr> &&_l, std::unique_ptr<expr> &&_r)
 		: op(_op), lc(std::move(_l)), rc(std::move(_r)) {}
+	void print(std::ostream &os) const
+	{
+		os << '{';
+		switch (op)
+		{
+#define op_case(CMP_OP, print)\
+			case CMP_OP:\
+				os << print;\
+				break;
+			op_case(LT, "<")
+			op_case(LE, "<=")
+			op_case(GT, ">")
+			op_case(GE, ">=")
+			op_case(EQ, "==")
+			op_case(NE, "!=")
+		}
+		os << ' ' << *lc << ' ' << *rc << '}';
+	}
 };
 struct bool_and : bool_expr
 {
 	std::unique_ptr<bool_expr> lc, rc;
 	bool_and(std::unique_ptr<bool_expr> &&_l, std::unique_ptr<bool_expr> &&_r)
 		: lc(std::move(_l)), rc(std::move(_r)) {}
+	void print(std::ostream &os) const
+	{
+		os << "{and " << *lc << ' ' << *rc << '}';
+	}
 };
 struct bool_or : bool_expr
 {
 	std::unique_ptr<bool_expr> lc, rc;
 	bool_or(std::unique_ptr<bool_expr> &&_l, std::unique_ptr<bool_expr> &&_r)
 		: lc(std::move(_l)), rc(std::move(_r)) {}
+	void print(std::ostream &os) const
+	{
+		os << "{or " << *lc << ' ' << *rc << '}';
+	}
 };
 /*
- * {B0} ::= {expr} @cmp_op {expr}
- * {B1} ::= {B0} | {B0} && {B0}
- * {bool_expr} ::= {B1} | {B1} || {B1}
+ * {B0} ::= {expr} @cmp_op {expr} | ({bool_expr})
+ * {B1} ::= {B0} | {B1} && {B0}
+ * {bool_expr} ::= {B1} | {B2} || {B1}
  */
 using parse_bool_result = std::pair<std::unique_ptr<bool_expr>, expr_ite>;
 parse_bool_result parse_bool_expr(expr_ite first, const expr_ite &last);
@@ -243,12 +304,12 @@ parse_bool_result parse_b0(expr_ite first, const expr_ite &last)
 	{
 		++first;
 		auto ret = parse_bool_expr(first, last);
+		first = ret.second;
 		if (first < last && *first == ')')
 			return parse_bool_result(std::move(ret.first), first + 1);
 		throw ":-( expected ')'";
 	}
-	auto tmp_1 = parse_expr(first, last);
-	first = tmp_1.second;
+	auto &&expr_1 = parse_expr(first, last);
 	skip_space(first, last);
 	if (first >= last - 1) // op and another {expr}, at least 2 chars
 		throw "invalid bool expression.";
@@ -300,10 +361,10 @@ parse_bool_result parse_b0(expr_ite first, const expr_ite &last)
 		default:
 			throw "Invalid cmp op.";
 	}
-	auto tmp_2 = parse_expr(first, last);
+	auto expr_2 = parse_expr(first, last);
 	return parse_bool_result(std::make_unique<cmp>
-			(op, std::move(tmp_1.first), std::move(tmp_2.first)),
-			tmp_2.second);
+			(op, std::move(expr_1), std::move(expr_2)),
+			first);
 }
 parse_bool_result parse_b1(expr_ite first, const expr_ite &last)
 {
