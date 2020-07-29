b2c58e004b310e381ab5bb9c56b836cfd49fe800
xukl <xu2k3l4@outlook.com>
2020-07-29 19:00:17 +0800

    DEVELOP translate (under construction)

--- src/translate.cc
+++ src/translate.cc
@@ -15,7 +15,7 @@ convert_val_expr(const std::unique_ptr<expr::expr> &e, int &target,
 		throw "subscript is not supported yet.";
 	if (type == typeid(expr::id))
 	{
-		const std::string &var = static_cast<const expr::id&>(*e).id_name;
+		const std::string &var = static_cast<expr::id&>(*e).id_name;
 		if (regs.reg_map.count(var) == 0)
 			throw "Unknown identifier.";
 		target = regs.reg_map[var];
@@ -27,7 +27,7 @@ convert_val_expr(const std::unique_ptr<expr::expr> &e, int &target,
 	std::vector<instruction> ret;
 	if (type == typeid(expr::imm_num))
 	{
-		const int &imm = static_cast<const expr::imm_num&>(*e).value;
+		const int &imm = static_cast<expr::imm_num&>(*e).value;
 		int high = imm & 0xfffff000, low = imm & 0xfff;
 		if (imm == (imm << 20 >> 20))
 			ret = { instruction{inst_op::ADDI, zero, 0, imm, ans} };
@@ -39,13 +39,13 @@ convert_val_expr(const std::unique_ptr<expr::expr> &e, int &target,
 	}
 	else if (type == typeid(expr::neg))
 	{
-		const expr::neg &neg_expr = static_cast<const expr::neg&>(*e);
+		const expr::neg &neg_expr = static_cast<expr::neg&>(*e);
 		ret = convert_val_expr(neg_expr.c, ans, regs);
 		ret.push_back(instruction{inst_op::SUB, zero, ans, 0, ans});
 	}
 	else
 	{
-		const expr::bin_op &bin_expr = static_cast<const expr::bin_op&>(*e);
+		const expr::bin_op &bin_expr = static_cast<expr::bin_op&>(*e);
 		int lhs = UNDETERMINED_REG, rhs = UNDETERMINED_REG;
 		ret = convert_val_expr(bin_expr.lc, lhs, regs);
 		auto &&ret_rhs = convert_val_expr(bin_expr.rc, rhs, regs);
@@ -92,7 +92,7 @@ convert_bool_expr(const std::unique_ptr<expr::expr> &e, int &target,
 			&& type != typeid(expr::bool_and)
 			&& type != typeid(expr::bool_or))
 		throw "Error when convert_bool_expr: get val expr where bool expr is expected.";
-	const expr::bin_op &bin_expr = static_cast<const expr::bin_op&>(*e);
+	const expr::bin_op &bin_expr = static_cast<expr::bin_op&>(*e);
 	std::vector<instruction> ret, ret_rhs;
 	int lhs = UNDETERMINED_REG, rhs = UNDETERMINED_REG;
 	if (type == typeid(expr::cmp))
@@ -122,7 +122,7 @@ convert_bool_expr(const std::unique_ptr<expr::expr> &e, int &target,
 	}
 	if (type == typeid(expr::cmp))
 	{
-		const expr::cmp &cmp_expr = static_cast<const expr::cmp&>(*e);
+		const expr::cmp &cmp_expr = static_cast<expr::cmp&>(*e);
 		switch (cmp_expr.op)
 		{
 			case expr::cmp::LT:
@@ -157,6 +157,103 @@ convert_bool_expr(const std::unique_ptr<expr::expr> &e, int &target,
 				-4 * (target - REAL_REG + 1), 0});
 	regs.deallocate_reg(lhs);
 	regs.deallocate_reg(rhs);
+	return ret;
+}
+obj_code trarnslate_to_obj_code(const basic_block::cfg_type &cfg)
+{
+	virtual_reg reg_map;
+	obj_code ret;
+	for (const auto &[line, block] : cfg)
+	{
+		std::vector<instruction> inst;
+		for (const auto &sent : block.commands)
+		{
+			const auto &type = typeid(sent);
+			std::vector<instruction> sent_inst;
+			if (type == typeid(statement::LET))
+			{
+				const statement::assignment &assign =
+					static_cast<statement::LET&>(*sent).assign;
+				if (typeid(*(assign.val)) == typeid(expr::subscript))
+					throw "subscript is not supported yet.";
+				if (typeid(*(assign.var)) != typeid(expr::id))
+					throw "lvalue expected in {LET} command.";
+				int mem_reg = reg_map.preserve_var
+					(static_cast<expr::id&>(*(assign.var)).id_name, 1);
+				sent_inst = convert_val_expr(assign.val, mem_reg, reg_map);
+			}
+			else if (type == typeid(statement::INPUT))
+			{
+				const auto &inputs =
+					static_cast<statement::INPUT&>(*sent).inputs;
+				for (const auto &var : inputs)
+				{
+					const auto &type_var = typeid(*var);
+					if (type_var == typeid(expr::subscript))
+						throw "subscript is not supported yet.";
+					if (type_var != typeid(expr::id))
+						throw "lvalue expected in {INPUT} command.";
+					const auto &name = static_cast<expr::id&>(*var).id_name;
+					auto mem_reg = reg_map.preserve_var(name, 1);
+					sent_inst.insert(sent_inst.end(), {
+						instruction{inst_op::ADDI, zero, 0, CALL_READ, a0},
+						instruction{inst_op::INPUT, 0, 0, 0, 0},
+						instruction{inst_op::SW, sp, a0,
+							-4 * (mem_reg - REAL_REG + 1), 0}
+					});
+				}
+			}
+			else if (type == typeid(statement::EXIT))
+			{
+				int exit_val_reg = a1;
+				reg_map.preserve_reg(a1);
+				sent_inst = convert_val_expr
+					(static_cast<statement::EXIT&>(*sent).val,
+						exit_val_reg, reg_map);
+				sent_inst.insert(sent_inst.end(), {
+					instruction{inst_op::ADDI, zero, 0, CALL_EXIT, a0},
+					instruction{inst_op::EXIT, 0, 0, 0, 0}
+				});
+				reg_map.deallocate_reg(a1);
+			}
+			else if (type == typeid(statement::IF))
+			{
+				int branch_flag = a0;
+				reg_map.preserve_reg(a0);
+				sent_inst = convert_bool_expr
+					(static_cast<statement::IF&>(*sent).condition,
+						branch_flag, reg_map);
+				reg_map.deallocate_reg(a0);
+			}
+			else if (type == typeid(statement::FOR))
+			{
+				int branch_flag = a0;
+				reg_map.preserve_reg(a0);
+				sent_inst = convert_bool_expr
+					(static_cast<statement::FOR&>(*sent).condition,
+						branch_flag, reg_map);
+				reg_map.deallocate_reg(a0);
+			}
+			else if (type == typeid(statement::END_FOR))
+			{
+				const statement::assignment &assign =
+					static_cast<statement::END_FOR&>(*sent).step_statement;
+				if (typeid(*(assign.val)) == typeid(expr::subscript))
+					throw "subscript is not supported yet.";
+				if (typeid(*(assign.var)) != typeid(expr::id))
+					throw "lvalue expected in {LET} command.";
+				int mem_reg = reg_map.preserve_var
+					(static_cast<expr::id&>(*(assign.var)).id_name, 1);
+				sent_inst = convert_val_expr(assign.val, mem_reg, reg_map);
+			}
+			else
+			{
+			}
+		}
+		ret.emplace(line,
+				obj_code_block(std::move(inst), block.condition->deep_copy(),
+					block.jump_true, block.jump_false));
+	}
 	return ret;
 }
 }
--- src/translate.hpp
+++ src/translate.hpp
@@ -5,9 +5,11 @@
 #include <map>
 #include <set>
 #include <vector>
+#include <cassert>
 namespace translate
 {
 enum class inst_op { ADD, SUB, MUL, DIV, ADDI, LUI, ORI, LW, SW, JAL, INPUT, EXIT, AND, OR, SLTIU, SLT, BEQ, BNE, XORI };
+const int CALL_EXIT = 0, CALL_READ = 1, CALL_PRINT = 2;
 struct instruction
 {
 	inst_op op;
@@ -56,7 +58,7 @@ std::ostream &operator<< (std::ostream &os, const instruction &x)
 	x.print(os);
 	return os;
 }
-const int zero = 0, sp = 2, t0 = 5, t1 = 6, t2 = 7;
+const int zero = 0, sp = 2, t0 = 5, t1 = 6, t2 = 7, a0 = 10, a1 = 11;
 const static int REAL_REG = 32;
 struct virtual_reg
 {
@@ -68,19 +70,27 @@ struct virtual_reg
 		for (int i = 10; i < REAL_REG; ++i)
 			aval_reg.insert(i);
 	}
-	void preserve_var(const std::string &var_name, int elements)
+	int preserve_var(const std::string &var_name, int elements)
 	{
+		if (reg_map.count(var_name) != 0)
+			return reg_map[var_name];
+		int ret = memory_reg_end;
 		reg_map[var_name] = memory_reg_end;
 		virtual_reg_cnt += elements;
 		memory_reg_end += elements;
+		return ret;
+	}
+	void preserve_reg(int reg)
+	{
+		assert(aval_reg.count(reg) != 0);
+		aval_reg.erase(reg);
 	}
 	int allocate_reg()
 	{
 		if (aval_reg.empty())
 			return ++virtual_reg_cnt;
-		auto aval_it = aval_reg.begin();
-		int ret = *aval_it;
-		aval_reg.erase(aval_it);
+		int ret = *aval_reg.begin();
+		preserve_reg(ret);
 		return ret;
 	}
 	void deallocate_reg(int reg)
@@ -89,6 +99,19 @@ struct virtual_reg
 			aval_reg.insert(reg);
 	}
 };
+struct obj_code_block
+{
+	std::vector<instruction> instructions;
+	std::unique_ptr<expr::expr> condition;
+	int jump_true, jump_false;
+	obj_code_block(std::vector<instruction> &&_insts,
+			std::unique_ptr<expr::expr> &&cond,
+			int j_true, int j_false)
+		: instructions(std::move(_insts)), condition(std::move(cond)),
+		jump_true(j_true), jump_false(j_false) {}
+};
+using obj_code = std::map<int, obj_code_block>;
+obj_code trarnslate_to_obj_code(const basic_block::cfg_type &cfg);
 const int UNDETERMINED_REG = -1;
 std::vector<instruction>
 convert_val_expr(const std::unique_ptr<expr::expr> &e, int &target,
