3b0f752df5b9301aa116811729e362bfeaa05e58
xukl <xu2k3l4@outlook.com>
2020-07-31 09:50:34 +0800

    minor change: mv virtual_reg from .hpp to .cc

--- src/translate.cc
+++ src/translate.cc
@@ -2,6 +2,45 @@
 #include <typeinfo>
 namespace translate
 {
+using namespace inst;
+struct virtual_reg
+{
+	int virtual_reg_cnt, memory_reg_end;
+	std::set<int> aval_reg;
+	std::map<std::string, int> reg_map;
+	virtual_reg() : virtual_reg_cnt(REAL_REG), memory_reg_end(REAL_REG)
+	{
+		for (int i = 10; i < REAL_REG; ++i)
+			aval_reg.insert(i);
+	}
+	int preserve_var(const std::string &var_name, int elements)
+	{
+		if (reg_map.count(var_name) != 0)
+			return reg_map[var_name];
+		int ret = memory_reg_end;
+		reg_map[var_name] = memory_reg_end;
+		virtual_reg_cnt += elements;
+		memory_reg_end += elements;
+		return ret;
+	}
+	void preserve_reg(int reg)
+	{
+		aval_reg.erase(reg);
+	}
+	int allocate_reg()
+	{
+		if (aval_reg.empty())
+			return ++virtual_reg_cnt;
+		int ret = *aval_reg.begin();
+		preserve_reg(ret);
+		return ret;
+	}
+	void deallocate_reg(int reg)
+	{
+		if ((reg >= 10 && reg < REAL_REG) || reg > memory_reg_end)
+			aval_reg.insert(reg);
+	}
+};
 const int UNDETERMINED_REG = -1;
 std::vector<instruction>
 convert_val_expr(const std::unique_ptr<expr::expr> &e, int &target,
--- src/translate.hpp
+++ src/translate.hpp
@@ -8,51 +8,12 @@
 #include <vector>
 namespace translate
 {
-using namespace inst;
-struct virtual_reg
-{
-	int virtual_reg_cnt, memory_reg_end;
-	std::set<int> aval_reg;
-	std::map<std::string, int> reg_map;
-	virtual_reg() : virtual_reg_cnt(REAL_REG), memory_reg_end(REAL_REG)
-	{
-		for (int i = 10; i < REAL_REG; ++i)
-			aval_reg.insert(i);
-	}
-	int preserve_var(const std::string &var_name, int elements)
-	{
-		if (reg_map.count(var_name) != 0)
-			return reg_map[var_name];
-		int ret = memory_reg_end;
-		reg_map[var_name] = memory_reg_end;
-		virtual_reg_cnt += elements;
-		memory_reg_end += elements;
-		return ret;
-	}
-	void preserve_reg(int reg)
-	{
-		aval_reg.erase(reg);
-	}
-	int allocate_reg()
-	{
-		if (aval_reg.empty())
-			return ++virtual_reg_cnt;
-		int ret = *aval_reg.begin();
-		preserve_reg(ret);
-		return ret;
-	}
-	void deallocate_reg(int reg)
-	{
-		if ((reg >= 10 && reg < REAL_REG) || reg > memory_reg_end)
-			aval_reg.insert(reg);
-	}
-};
 struct obj_code_block
 {
-	std::vector<instruction> instructions;
+	std::vector<inst::instruction> instructions;
 	std::unique_ptr<expr::expr> condition;
 	int jump_true, jump_false;
-	obj_code_block(std::vector<instruction> &&_insts,
+	obj_code_block(std::vector<inst::instruction> &&_insts,
 			std::unique_ptr<expr::expr> &&cond,
 			int j_true, int j_false)
 		: instructions(std::move(_insts)), condition(std::move(cond)),
